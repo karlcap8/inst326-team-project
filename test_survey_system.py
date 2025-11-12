dc67fb40104fe2e1dc58257bc48b34b96076ce9d
hkhalsa1 <hkhalsa@terpmail.umd.edu>
2025-11-12 13:33:35 -0500

    Update test_survey_system.py

--- test_survey_system.py
+++ test_survey_system.py
@@ -1,3 +1,4 @@
 test_validation_report_passes_when_no_issues() # not implemented yet - Sukhman 
 test_pii_remover_drops_columns() # not implemented yet - Harrang
 test_type_caster_casts_types() # not implemented yet - Harrang
+test_pipeline_runs_all_steps_in_order() # not implemented yet - Was Aaron's part relating to demo/pipeline
