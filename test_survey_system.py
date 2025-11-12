d88e5f80dc910bb7b6ed6a3f9abf5ba650dcb76a
hkhalsa1 <hkhalsa@terpmail.umd.edu>
2025-11-12 13:31:16 -0500

    Update test_survey_system.py

--- test_survey_system.py
+++ test_survey_system.py
@@ -1,1 +1,3 @@
 test_validation_report_passes_when_no_issues() # not implemented yet - Sukhman 
+test_pii_remover_drops_columns() # not implemented yet - Harrang
+test_type_caster_casts_types() # not implemented yet - Harrang
