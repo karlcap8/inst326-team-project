1ff3520e3a7a4a0fd102d094161cba482b2dfcaf
karlcap8 <karl.capili8@gmail.com>
2025-11-16 19:13:59 -0500

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,11 @@
-
 class Pipeline:
- 	def __init__(self, steps):
-     	self.steps = steps
-     	self.history = []
+    def __init__(self, steps):
+        self.steps = steps
+        self.history = []
 
- def run(self, df):
-     	out = df.copy()
-     	for step in self.steps:
-         	out = step.apply(out)
-             self.history.extend(step.history())
-     	return out
+    def run(self, df):
+        out = df.copy()
+        for step in self.steps:
+            out = step.apply(out)
+            self.history.extend(step.history())
+        return out
